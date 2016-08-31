bb18fb28d0684dcd05092dae5d0f7a884cca2278
gustavoneves12 <gustavo.neves12@gmail.com>
2016-08-31 10:48:27 -0300

    convert sonar logs to images

---
+++ examples/convert_to_images.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <base/samples/Sonar.hpp>
+#include <boost/filesystem.hpp>
+#include "rock_util/LogReader.hpp"
+#include "rock_util/SonarSampleConverter.hpp"
+#include "sonar_target_tracking/Preprocessing.hpp"
+#include "base/test_config.h"
+
+using namespace sonar_target_tracking;
+
+int main(int argc, char **argv) {
+
+    const std::string logfiles[] = {
+        DATA_PATH_STRING + "/logs/gemini-jequitaia.0.log",
+        DATA_PATH_STRING +  "/logs/gemini-jequitaia.4.log",
+        DATA_PATH_STRING + "/logs/gemini-ferry.0.log",
+        DATA_PATH_STRING + "/logs/gemini-ferry.3.log"
+    };
+
+    uint32_t sz = sizeof(logfiles) / sizeof(std::string);
+
+    for (uint32_t i = 0; i < sz; i++) {
+        rock_util::LogReader reader(logfiles[i]);
+        rock_util::LogStream stream = reader.stream("gemini.sonar_samples");
+
+        boost::filesystem::create_directories(boost::filesystem::path("output"));
+        boost::filesystem::path p(logfiles[i]);
+
+        base::samples::Sonar sample;
+        while (stream.current_sample_index() < stream.total_samples()) {
+            stream.next<base::samples::Sonar>(sample);
+            cv::Mat src = cv::Mat(sample.bins).reshape(1, sample.beam_count);
+
+            cv::Rect roi = preprocessing::calc_horiz_roi(src, 0.075);
+            cv::Mat mat;
+            src(roi).convertTo(mat, CV_8U, 255);
+
+            preprocessing::remove_low_intensities_columns(mat, mat);
+
+            cv::imshow("mat", mat);
+
+            std::stringstream ss;
+            ss << "output/" << p.stem().string() << "-" << stream.current_sample_index() << ".png";
+
+            cv::imwrite(ss.str(), mat);
+            cv::waitKey(100);
+        }
+    }
+
+    return 0;
+}
